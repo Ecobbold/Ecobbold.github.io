310cff22ae333b399060aabbeea32762751d72e0
Emilie <ecobbold@rogers.ca>
2014-11-16 23:00:31 -0500

    Update Site
    Nov 16 2014

---
+++ loader.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ecobb_000 on 2014-11-05.
+ */
+//paste this code under head tag or in a seperate js file.
+// Wait for window load
+$(window).load(function() {
+
+    "use strict";
+
+    $('.status').fadeOut();
+    // Animate loader off screen
+    $(".loader").delay(350).fadeOut(2000);
+    });
